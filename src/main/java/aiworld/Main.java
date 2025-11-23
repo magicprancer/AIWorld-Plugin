5cae79d1c799bb8f9030b292839c09996aa06b7b
magicprancer <magicprancer3@outlook.com>
2025-11-23 14:07:27 -0600

    Update Main.java

--- src/main/java/aiworld/Main.java
+++ src/main/java/aiworld/Main.java
@@ -5,6 +5,9 @@ import aiworld.towns.TownsModule;
 import aiworld.survivors.SurvivorsModule;
 import aiworld.magic.MagicModule;
 import aiworld.admin.AdminModule;
+import aiworld.admin.AdminAI;
+import aiworld.commands.AINPCCommand;
+import aiworld.commands.AIAdminCommand;
 
 public class Main extends JavaPlugin {
     public TownsModule towns;
@@ -14,19 +17,14 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        // Save default config if it does not exist
         saveDefaultConfig();
 
-        // Initialize modules
+        // Initialize all modules
         towns = new TownsModule(this);
         survivors = new SurvivorsModule(this);
         magic = new MagicModule(this);
         admin = new AdminModule(this);
 
-        getLogger().info("AIWorld Plugin Enabled!");
-    }
-
-    @Override
-    public void onDisable() {
-        getLogger().info("AIWorld Plugin Disabled!");
-    }
-}
+        // Register commands
+        new

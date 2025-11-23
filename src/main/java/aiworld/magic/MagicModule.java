de21bb3197d117da8a9f99cf307917e99f512e28
magicprancer <magicprancer3@outlook.com>
2025-11-23 13:58:57 -0600

    Create MagicModule.java

---
+++ src/main/java/aiworld/magic/MagicModule.java
@@ -0,0 +1,14 @@
+package aiworld.magic;
+
+import aiworld.Main;
+import org.bukkit.event.Listener;
+
+public class MagicModule implements Listener {
+    private final Main plugin;
+
+    public MagicModule(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    // Future: add wizard NPCs, magical events, AI quests
+}

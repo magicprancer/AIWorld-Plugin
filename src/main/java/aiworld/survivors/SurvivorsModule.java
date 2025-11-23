938c4317b1b878afaaf63125377ba30a2519dc90
magicprancer <magicprancer3@outlook.com>
2025-11-23 13:58:30 -0600

    Create SurvivorsModule.java

---
+++ src/main/java/aiworld/survivors/SurvivorsModule.java
@@ -0,0 +1,14 @@
+package aiworld.survivors;
+
+import aiworld.Main;
+import org.bukkit.event.Listener;
+
+public class SurvivorsModule implements Listener {
+    private final Main plugin;
+
+    public SurvivorsModule(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    // Future: add survivor NPC behavior, base building, AI routines
+}

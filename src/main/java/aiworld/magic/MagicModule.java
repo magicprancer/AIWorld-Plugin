ed7bfd868ed1a8ec160310a8e43bafcd2c1b3fe3
magicprancer <magicprancer3@outlook.com>
2025-11-23 14:18:30 -0600

    Update MagicModule.java

--- src/main/java/aiworld/magic/MagicModule.java
+++ src/main/java/aiworld/magic/MagicModule.java
@@ -2,13 +2,23 @@ package aiworld.magic;
 
 import aiworld.Main;
 import org.bukkit.event.Listener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MagicModule implements Listener {
     private final Main plugin;
+    private final List<MagicNPC> magicNPCs;
 
     public MagicModule(Main plugin) {
         this.plugin = plugin;
+        this.magicNPCs = new ArrayList<>();
     }
 
-    // Future: add wizard NPCs, magical events, AI quests
+    public void addNPC(MagicNPC npc) {
+        magicNPCs.add(npc);
+    }
+
+    public List<MagicNPC> getAllNPCs() {
+        return new ArrayList<>(magicNPCs);
+    }
 }

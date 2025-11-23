f47e6a0aa860c3922043782d1ed7fa356e818d9d
magicprancer <magicprancer3@outlook.com>
2025-11-23 14:01:38 -0600

    Update Main.java

--- src/main/java/aiworld/Main.java
+++ src/main/java/aiworld/Main.java
@@ -1,10 +1,27 @@
 package aiworld;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import aiworld.towns.TownsModule;
+import aiworld.survivors.SurvivorsModule;
+import aiworld.magic.MagicModule;
+import aiworld.admin.AdminModule;
 
 public class Main extends JavaPlugin {
+    public TownsModule towns;
+    public SurvivorsModule survivors;
+    public MagicModule magic;
+    public AdminModule admin;
+
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+
+        // Initialize modules
+        towns = new TownsModule(this);
+        survivors = new SurvivorsModule(this);
+        magic = new MagicModule(this);
+        admin = new AdminModule(this);
+
         getLogger().info("AIWorld Plugin Enabled!");
     }
 

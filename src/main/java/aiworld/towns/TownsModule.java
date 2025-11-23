a792597d9f67b0fc5bf8ffbb4cb3f55e1a570918
magicprancer <magicprancer3@outlook.com>
2025-11-23 13:58:00 -0600

    Create TownsModule.java

---
+++ src/main/java/aiworld/towns/TownsModule.java
@@ -0,0 +1,14 @@
+package aiworld.towns;
+
+import aiworld.Main;
+import org.bukkit.event.Listener;
+
+public class TownsModule implements Listener {
+    private final Main plugin;
+
+    public TownsModule(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    // Future: add NPC creation, building, and routines
+}

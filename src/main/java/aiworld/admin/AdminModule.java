0f09c6999644cc2d05b9f7274d2d28c30e68942c
magicprancer <magicprancer3@outlook.com>
2025-11-23 13:59:39 -0600

    Create AdminModule.java

---
+++ src/main/java/aiworld/admin/AdminModule.java
@@ -0,0 +1,13 @@
+package aiworld.admin;
+
+import aiworld.Main;
+
+public class AdminModule {
+    private final Main plugin;
+
+    public AdminModule(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    // Future: add AI chat moderation, admin commands, announcements
+}
